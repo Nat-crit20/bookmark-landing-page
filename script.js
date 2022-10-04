be1921564054c35718109ac2a2a82e9030977667
Nathan Greuel <greuelnathan74@gmail.com>
2022-10-04 13:10:22 -0500

    Working on email validation

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const bookmarkTab = document.getElementById("bookmark-tab");
 const searchingTab = document.getElementById("searching-tab");
 const sharingTab = document.getElementById("sharing-tab");
+const email = document.getElementById("email");
+
 const form = document.querySelector("form");
 const bookmark = document.querySelector(".bookmarking");
 const searching = document.querySelector(".searching");
 const sharing = document.querySelector(".sharing");
-
+const errorMessage = document.querySelector(".error-msg");
 const question = document.querySelectorAll(".question-container");
-const activeAnswer = document.querySelector(".answer");
-
-const isEmail =
-  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
+const valid = document.querySelector(".isInValid");
 //Functions
-function isValidEmail(email) {
-  if (!isEmail.test(email)) {
-    throw new Error("Whoops make sure it's an email");
+function checkEmail(email) {
+  var filter =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!filter.test(email)) {
+    return false;
   }
+}
+
+function toggleError(border, visible, msg = "") {
+  email.style.border = border;
+  errorMessage.style.visibility = visible;
+  errorMessage.innerHTML = msg;
+  email.classList.add("isInValid");
   return;
 }
 
@@ -69,7 +76,26 @@ question.forEach((ask) => {
 bookmarkTab.addEventListener("click", toggleContent);
 searchingTab.addEventListener("click", toggleContent);
 sharingTab.addEventListener("click", toggleContent);
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(e.target.value);
+  const data = new FormData(form);
+  let emailData = data.get("email");
+  console.log(emailData);
+  email.classList.remove("isInValid");
+  if (email.value === "") {
+    toggleError(
+      `2px solid red`,
+      "visible",
+      "Whoops! It looks like you forgot to add your email"
+    );
+  } else if (checkEmail(emailData) === false) {
+    toggleError(
+      `2px solid red`,
+      "visible",
+      "Please provide a valid email address"
+    );
+  } else {
+    toggleError("2px solid white", "hidden");
+  }
 });
